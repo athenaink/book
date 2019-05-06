8ea3a5c50b22c2af442c2af1b41bbcd220699550
kenblikylee <ken@kendeMBP.lan>
2019-05-07 00:31:42 +0800

    更改静态页面发布分支为master

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cd _book
 git init
 git add -A
 git commit -m 'update book'
-git push -f https://github.com/athenaink/athenaink.github.io.git master:gh-pages
+git push -f https://github.com/athenaink/athenaink.github.io.git master:master
